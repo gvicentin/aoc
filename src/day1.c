cb62510e546e71d2f2b55daf7a88819022c18ffd
Guilherme Vicentin <gvicentin@ciandt.com>
2023-07-10 22:12:46 -0300

    Completed day one

--- src/day1.c
+++ src/day1.c
@@ -2,50 +2,102 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "minunit.h"
+
+char g_assertMsg[1024];
+int g_testsRun = 0;
+
+static void updateTopRank(unsigned int *top3, unsigned int caloriesCount);
+static void calculateTop3Calories(FILE *stream, unsigned int *top3);
+
+static char *testGetTop3Calories(void);
+static char *allTests(void);
+
+int main(int argc, char *argv[]) {
+    unsigned int top3Elfs[3] = {0};
+
+    if (argc > 1 && (strcmp(argv[1], "--test") || strcmp(argv[1], "-t"))) {
+        // run tests and exit
+        char *testsResults = allTests();
+        printf("Test count: %d\n", g_testsRun);
+        if (testsResults != 0) {
+            printf("Test failed\n");
+            printf("%s\n", testsResults);
+            return 1;
+        }
+        printf("All tests passed.\n");
+        return 0;
+    }
+
+    // finding the answer
+    calculateTop3Calories(stdin, top3Elfs);
+    printf("Total calories (top 1 Elf): %u\n", top3Elfs[0]);
+    printf("Total calories (top 3 Elfs): %u\n",
+           top3Elfs[0] + top3Elfs[1] + top3Elfs[2]);
+
+    return 0;
+}
+
+void updateTopRank(unsigned int *top3, unsigned int caloriesCount) {
+    if (caloriesCount > top3[0]) {
+        top3[2] = top3[1];
+        top3[1] = top3[0];
+        top3[0] = caloriesCount;
+    } else if (caloriesCount > top3[1]) {
+        top3[2] = top3[1];
+        top3[1] = caloriesCount;
+    } else if (caloriesCount > top3[2]) {
+        top3[2] = caloriesCount;
+    }
+}
+
 void calculateTop3Calories(FILE *stream, unsigned int *top3) {
     char lineBuff[64];
-    unsigned int mostCalories = 0, elfCaloriesCount = 0;
+    unsigned int elfCaloriesCount = 0;
 
     while (fgets(lineBuff, sizeof(lineBuff), stream) != NULL) {
         if (strlen(lineBuff) == 1) {
             // found an empty line
+            updateTopRank(top3, elfCaloriesCount);
             elfCaloriesCount = 0;
-            continue;
         }
 
         int calories = atoi(lineBuff);
         elfCaloriesCount += calories;
-
-        if (elfCaloriesCount > mostCalories) {
-            mostCalories = elfCaloriesCount;
-        }
     }
 
-    top3[0] = mostCalories;
+    updateTopRank(top3, elfCaloriesCount);
 }
 
-int main(int argc, char *argv[]) {
-    FILE *fp;
-    int shouldCloseFile = 1;
-    unsigned int top3[3];
+static char *testGetTop3Calories(void) {
+    FILE *tempFile;
+    char *testInput =
+        "1000\n2000\n3000\n\n4000\n\n5000\n"
+        "6000\n\n7000\n8000\n9000\n\n10000\n";
 
-    if (argc > 1) {
-        fp = fopen(argv[1], "r");
-        if (fp == NULL) {
-            fprintf(stderr, "Failed to open file %s\n", argv[1]);
-            return 1;
-        }
-    } else {
-        fp = stdin;
-        shouldCloseFile = 0;
+    tempFile = fopen("/tmp/day1_test.in", "w+");
+    if (tempFile == NULL) {
+        fprintf(stderr, "Failed to create temp file\n");
+        exit(1);
     }
 
-    calculateTop3Calories(fp, top3);
-    printf("%u\n", top3[0]);
+    fwrite(testInput, sizeof(testInput[0]), strlen(testInput), tempFile);
+    rewind(tempFile);
 
-    if (shouldCloseFile) {
-        fclose(fp);
-    }
+    unsigned int top3[3] = {0};
+    calculateTop3Calories(tempFile, top3);
 
-    return 0;
+    MU_ASSERT_FMT(24000 == top3[0], "Top 1 calories should be %u, but got %u", 2400,
+                  top3[0]);
+    MU_ASSERT_FMT(45000 == top3[0] + top3[1] + top3[2],
+                  "Top 3 calories total should be %u, but got %u", 45000,
+                  top3[0] + top3[1] + top3[2]);
+
+    fclose(tempFile);
+    MU_PASS;
+}
+
+static char *allTests(void) {
+    MU_TEST(testGetTop3Calories);
+    MU_PASS;
 }
