fe454c5ddaa3d52038500cbff57c463e3a7594b4
Guilherme Vicentin <gvicentin@ciandt.com>
2023-07-24 15:52:41 -0300

    Day 4 part two

--- src/day4.c
+++ src/day4.c
@@ -8,9 +8,10 @@ char g_assertMsg[1024];
 int g_testsRun = 0;
 
 static int assignmentPairContains(int x1, int x2, int y1, int y2);
-static int countAssignmentPairContained(void);
+static int assignmentPairOverlaps(int x1, int x2, int y1, int y2);
+static void countAssignmentPairs(int *contains, int *overlaps);
 
-static char *testAssignmentPairContains(void);
+static char *testAssignmentPair(void);
 static char *allTests(void);
 
 int main(int argc, char *argv[]) {
@@ -27,8 +28,10 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    int count = countAssignmentPairContained();
-    printf("Count: %d\n", count);
+    int countContains, countOverlaps;
+    countAssignmentPairs(&countContains, &countOverlaps);
+    printf("Count contains: %d\n", countContains);
+    printf("Count overlaps: %d\n", countOverlaps);
 
     return 0;
 }
@@ -37,40 +40,54 @@ static int assignmentPairContains(int x1, int x2, int y1, int y2) {
     return (x1 <= y1 && x2 >= y2) || (y1 <= x1 && y2 >= x2);
 }
 
-static int countAssignmentPairContained(void) {
-    int x1, x2, y1, y2;
-    int count = 0;
-
-    while (scanf("%d-%d,%d-%d", &x1, &x2, &y1, &y2) != EOF) {
-        int result = assignmentPairContains(x1, x2, y1, y2);
-        count += result;
-    }
-
-    return count;
+static int assignmentPairOverlaps(int x1, int x2, int y1, int y2) {
+    return (y1 <= x2 && y1 >= x1) || (x1 <= y2 && x1 >= y1);
 }
 
-static char *testAssignmentPairContains(void) {
-    int input[] = {
-        2, 4, 6, 8,  // One entry per line
-        2, 3, 4, 5,  //
-        5, 7, 7, 9,  //
-        2, 8, 3, 7,  //
-        6, 6, 4, 6,  //
-        2, 6, 4, 8,
-    };
-    int expected[] = {0, 0, 0, 1, 1, 0};
+static void countAssignmentPairs(int *contains, int *overlaps) {
+    int x1, x2, y1, y2;
+    *contains = 0;
+    *overlaps = 0;
+
+    while (scanf("%d-%d,%d-%d", &x1, &x2, &y1, &y2) != EOF) {
+        int resultContains = assignmentPairContains(x1, x2, y1, y2);
+        int resultOverlaps = assignmentPairOverlaps(x1, x2, y1, y2);
+        *contains += resultContains;
+        *overlaps += resultOverlaps;
+    }
+}
+
+static char *testAssignmentPair(void) {
+    int input[] = {2, 4, 6, 8,  // One entry per line
+                   2, 3, 4, 5,  //
+                   5, 7, 7, 9,  //
+                   2, 8, 3, 7,  //
+                   6, 6, 4, 6,  //
+                   2, 6, 4, 8, 82, 93, 4, 81};
+    int expectedContains[] = {0, 0, 0, 1, 1, 0, 0};
+    int expectedOverlaps[] = {0, 0, 1, 1, 1, 1, 0};
     int index = 0;
 
-    for (int i = 0; i < sizeof(expected)/sizeof(expected[0]); ++i) {
-        int result = assignmentPairContains(input[index], input[index + 1], input[index + 2], input[index + 3]);
+    for (int i = 0; i < sizeof(expectedContains) / sizeof(expectedContains[0]); ++i) {
+        int resultContains = assignmentPairContains(input[index], input[index + 1],
+                                                    input[index + 2], input[index + 3]);
+        int resultOverlaps = assignmentPairOverlaps(input[index], input[index + 1],
+                                                    input[index + 2], input[index + 3]);
+        MU_ASSERT_FMT(expectedContains[i] == resultContains,
+                      "[%d-%d,%d-%d] Expect contains (%d) but got (%d)", input[index],
+                      input[index + 1], input[index + 2], input[index + 3],
+                      expectedContains[i], resultContains);
+        MU_ASSERT_FMT(expectedOverlaps[i] == resultOverlaps,
+                      "[%d-%d,%d-%d] Expect overlaps (%d) but got (%d)", input[index],
+                      input[index + 1], input[index + 2], input[index + 3],
+                      expectedOverlaps[i], resultOverlaps);
         index += 4;
-        MU_ASSERT_FMT(expected[i] == result, "Expect (%d) but got (%d)", expected[i], result);
     }
 
     MU_PASS;
 }
 
 static char *allTests(void) {
-    MU_TEST(testAssignmentPairContains);
+    MU_TEST(testAssignmentPair);
     MU_PASS;
 }
